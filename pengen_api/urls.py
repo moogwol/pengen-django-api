0dc77aab104a7e05901094698289b2731e64eb73
Miguel Corkhill <moogwol@msn.com>
2021-11-22 21:13:06 +0100

    now able to pass get parameters to the api

--- pengen_api/urls.py
+++ pengen_api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('api/drawings', views.DrawingsList.as_view()),
     path('api/drawings/<int:pk>', views.PostRetrieveDeleteDrawing.as_view()),
-    path('api/drawing/<title>', views.RetrieveDrawing.as_view()),
+    # path('api/drawing/<title>', views.RetrieveDrawing.as_view()),
+    path('api/drawing', views.RetrieveADrawing.as_view()),
 ]
--- pengen_api/views.py
+++ pengen_api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView, ListAPIView
 from rest_framework.response import Response
 
 from .models import Drawing
@@ -17,13 +17,21 @@ class PostRetrieveDeleteDrawing(RetrieveUpdateDestroyAPIView):
     serializer_class = DrawingSerializer
 
 
-class RetrieveDrawing(RetrieveAPIView):
-    queryset = Drawing.objects.all()
+# class RetrieveDrawing(RetrieveAPIView):
+#     queryset = Drawing.objects.all()
+#     serializer_class = DrawingSerializer
+#     lookup_field = 'title'
+
+class RetrieveADrawing(ListAPIView):
     serializer_class = DrawingSerializer
-    lookup_field = 'title'
 
+    def get_queryset(self):
+        queryset = Drawing.objects.all()
 
+        title = self.request.query_params.get('title')
+        queryset = queryset.filter(title=title)
 
+        return queryset
 
 
 
