5b3c2fc0f9612623ac0c9b7d0ae88c4ff5728efc
Miguel Corkhill <moogwol@msn.com>
2021-11-22 14:51:20 +0100

    added url to retrieve a single drawing

--- pengen_api/urls.py
+++ pengen_api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('api/drawings', views.DrawingsList.as_view()),
     path('api/drawings/<int:pk>', views.PostRetrieveDeleteDrawing.as_view()),
+    path('api/drawing/<title>', views.RetrieveDrawing.as_view()),
 ]
--- pengen_api/views.py
+++ pengen_api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView
 from rest_framework.response import Response
 
 from .models import Drawing
@@ -15,6 +15,16 @@ class DrawingsList(ListCreateAPIView):
 class PostRetrieveDeleteDrawing(RetrieveUpdateDestroyAPIView):
     queryset = Drawing.objects.all()
     serializer_class = DrawingSerializer
+
+
+class RetrieveDrawing(RetrieveAPIView):
+    queryset = Drawing.objects.all()
+    serializer_class = DrawingSerializer
+    lookup_field = 'title'
+
+
+
+
 
 
 
